4ac2aaa0cce22b889f8dd5101ebe5c07f044b65b
arefkoro <56038195+arefkoro@users.noreply.github.com>
2022-01-17 14:00:39 +0100

    .

---
+++ FIS/KBE/dev/kbe-dev.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 0.13.3"
+  required_providers {
+    ibm = {
+      source  = "ibm-cloud/ibm"
+      version = "1.12.0"
+    }
+  }
+}
+
+provider "ibm" {
+  generation         = 2
+  region             = "eu-de"
+}
+
+module "ibm_is_vpc" {
+  source = "../../../Base/"
+}
+
+module "ibm_is_instance" {
+  source = "../../../Base/"
+}
